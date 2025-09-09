1c1361eeb4ab7f2b954fb9a30141588eb0ae00ab
Abradshaw1 <148660157+Abradshaw1@users.noreply.github.com>
2025-09-09 00:15:53 -0700

    Fix header component text properly

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -2,6 +2,7 @@
        <div className="space-y-6 flex-grow">
 -        <h1 className="text-5xl font-bold text-gray-900">
 -          Aidan Bradshaw
+          Aidan Bradshaw - TEST CHANGE
 -        </h1>
 +        <h1 className="text-5xl font-bold text-gray-900">
 +          Aidan Bradshaw - TEST CHANGE
