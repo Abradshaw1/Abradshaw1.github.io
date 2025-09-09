dd2b4842d4f65c6a08591177679a373a0ab7dc5c
Abradshaw1 <148660157+Abradshaw1@users.noreply.github.com>
2025-09-09 00:21:20 -0700

    Updated Header.tsx

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,10 +1,9 @@
-@@ .. @@
        <div className="space-y-6 flex-grow">
--        <h1 className="text-5xl font-bold text-gray-900">
--          Aidan Bradshaw
+        <h1 className="text-5xl font-bold text-gray-900">
+          Aidan Bradshaw
           Aidan Bradshaw - TEST CHANGE
--        </h1>
-+        <h1 className="text-5xl font-bold text-gray-900">
-+          Aidan Bradshaw - TEST CHANGE
-+        </h1>
+        </h1>
+        <h1 className="text-5xl font-bold text-gray-900">
+          Aidan Bradshaw - TEST CHANGE
+        </h1>
          <div className="space-y-1">
